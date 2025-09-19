e2a3783d00f2b2bf21ee60397bcd4f66edfe1849
antlers <antlers@illucid.net>
2025-09-18 17:43:17 -0700

    move test corpus helpers into CorpusExt

--- src/adaptive_corpus.rs
+++ src/adaptive_corpus.rs
@@ -235,111 +235,91 @@ where
 }
 
 #[cfg(test)]
-fn count_char(corpus: &Corpus, c: char) -> u32 {
-    corpus.chars[corpus.corpus_char(c)]
-}
-
-#[cfg(test)]
-fn count_bigram(corpus: &Corpus, bg: [char; 2]) -> u32 {
-    corpus.bigrams[corpus.corpus_bigram(&bg)]
-}
-
-#[cfg(test)]
-fn count_trigram(corpus: &Corpus, tg: [char; 3]) -> u32 {
-    corpus.trigrams[corpus.corpus_trigram(&tg)]
-}
-
-#[cfg(test)]
-fn count_skipgram(corpus: &Corpus, sg: [char; 2]) -> u32 {
-    corpus.skipgrams[corpus.corpus_bigram(&sg)]
-}
-
-#[cfg(test)]
 fn verify_corpus_si_pre(corpus: Corpus) {
     // Monograms
-    assert_eq!(count_char(&corpus, 'e'), 50497522);
-    assert_eq!(count_char(&corpus, '†'), 0);
+    assert_eq!(corpus.count_char('e'), 50497522);
+    assert_eq!(corpus.count_char('†'), 0);
 
     // // Bigrams
-    assert_eq!(count_bigram(&corpus, ['h', 'e']), 8729312);
-    assert_eq!(count_bigram(&corpus, ['h', '†']), 0);
+    assert_eq!(corpus.count_bigram(['h', 'e']), 8729312);
+    assert_eq!(corpus.count_bigram(['h', '†']), 0);
 
     // Trigrams
-    assert_eq!(count_trigram(&corpus, ['t', 'h', 'e']), 6802477);
-    assert_eq!(count_trigram(&corpus, ['t', 'h', '†']), 0);
-    assert_eq!(count_trigram(&corpus, ['h', 'e', ' ']), 5421447);
-    assert_eq!(count_trigram(&corpus, ['h', '†', ' ']), 0);
-    assert_eq!(count_trigram(&corpus, ['e', ' ', 'q']), 40503);
-    assert_eq!(count_trigram(&corpus, ['†', ' ', 'q']), 0);
-    assert_eq!(count_trigram(&corpus, ['e', ' ', 'l']), 438196);
-    assert_eq!(count_trigram(&corpus, ['†', ' ', 'l']), 0);
+    assert_eq!(corpus.count_trigram(['t', 'h', 'e']), 6802477);
+    assert_eq!(corpus.count_trigram(['t', 'h', '†']), 0);
+    assert_eq!(corpus.count_trigram(['h', 'e', ' ']), 5421447);
+    assert_eq!(corpus.count_trigram(['h', '†', ' ']), 0);
+    assert_eq!(corpus.count_trigram(['e', ' ', 'q']), 40503);
+    assert_eq!(corpus.count_trigram(['†', ' ', 'q']), 0);
+    assert_eq!(corpus.count_trigram(['e', ' ', 'l']), 438196);
+    assert_eq!(corpus.count_trigram(['†', ' ', 'l']), 0);
 
     // Skipgrams
-    assert_eq!(count_skipgram(&corpus, ['t', 'e']), 7955798);
-    assert_eq!(count_skipgram(&corpus, ['t', '†']), 0);
-    assert_eq!(count_skipgram(&corpus, ['e', 'q']), 45074);
-    assert_eq!(count_skipgram(&corpus, ['†', 'q']), 0);
-    assert_eq!(count_skipgram(&corpus, ['e', 'l']), 1300716);
-    assert_eq!(count_skipgram(&corpus, ['†', 'l']), 0);
+    assert_eq!(corpus.count_skipgram(['t', 'e']), 7955798);
+    assert_eq!(corpus.count_skipgram(['t', '†']), 0);
+    assert_eq!(corpus.count_skipgram(['e', 'q']), 45074);
+    assert_eq!(corpus.count_skipgram(['†', 'q']), 0);
+    assert_eq!(corpus.count_skipgram(['e', 'l']), 1300716);
+    assert_eq!(corpus.count_skipgram(['†', 'l']), 0);
 
     // Everything else the same
-    assert_eq!(count_bigram(&corpus, ['v', 'e']), 2978051);
-    assert_eq!(count_bigram(&corpus, ['e', 'r']), 6377359);
-    assert_eq!(count_trigram(&corpus, ['o', 'v', 'e']), 496824);
-    assert_eq!(count_trigram(&corpus, ['v', 'e', 'r']), 934355);
-    assert_eq!(count_trigram(&corpus, ['e', 'r', ' ']), 2089364);
-    assert_eq!(count_skipgram(&corpus, ['o', 'e']), 3663662);
-    assert_eq!(count_skipgram(&corpus, ['e', ' ']), 10441564);
+    assert_eq!(corpus.count_bigram(['v', 'e']), 2978051);
+    assert_eq!(corpus.count_bigram(['e', 'r']), 6377359);
+    assert_eq!(corpus.count_trigram(['o', 'v', 'e']), 496824);
+    assert_eq!(corpus.count_trigram(['v', 'e', 'r']), 934355);
+    assert_eq!(corpus.count_trigram(['e', 'r', ' ']), 2089364);
+    assert_eq!(corpus.count_skipgram(['o', 'e']), 3663662);
+    assert_eq!(corpus.count_skipgram(['e', ' ']), 10441564);
 }
 
 #[cfg(test)]
 fn verify_corpus_si_post(corpus: Corpus) {
     // // Monograms
-    // assert_eq!(count_char(&corpus, 'e'), 41768210);
-    // assert_eq!(count_char(&corpus, '†'), 8729312);
+    // assert_eq!(corpus.count_char('e'), 41768210);
+    // assert_eq!(corpus.count_char('†'), 8729312);
 
     // // // Bigrams
-    // assert_eq!(count_bigram(&corpus, ['h', 'e']), 0);
-    // assert_eq!(count_bigram(&corpus, ['h', '†']), 8729312);
-    // assert_eq!(count_bigram(&corpus, ['†', 'e']), 40073);
-    // assert_eq!(count_bigram(&corpus, ['†', 'a']), 248245);
-    // assert_eq!(count_bigram(&corpus, ['†', 'i']), 253922);
-    // assert_eq!(count_bigram(&corpus, ['†', 'o']), 12705);
-    // assert_eq!(count_bigram(&corpus, ['†', ' ']), 5421447);
+    // assert_eq!(corpus.count_bigram(['h', 'e']), 0);
+    // assert_eq!(corpus.count_bigram(['h', '†']), 8729312);
+    // assert_eq!(corpus.count_bigram(['†', 'e']), 40073);
+    // assert_eq!(corpus.count_bigram(['†', 'a']), 248245);
+    // assert_eq!(corpus.count_bigram(['†', 'i']), 253922);
+    // assert_eq!(corpus.count_bigram(['†', 'o']), 12705);
+    // assert_eq!(corpus.count_bigram(['†', ' ']), 5421447);
 
     // Trigrams
-    assert_eq!(count_trigram(&corpus, ['t', 'h', 'e']), 0);
-    assert_eq!(count_trigram(&corpus, ['t', 'h', '†']), 6802477);
-    assert_eq!(count_trigram(&corpus, ['h', 'e', ' ']), 0);
-    assert_eq!(count_trigram(&corpus, ['h', '†', ' ']), 5421447);
-    assert_eq!(count_trigram(&corpus, ['e', ' ', 'q']), 21049);
-    assert_eq!(count_trigram(&corpus, ['†', ' ', 'q']), 19454);
-    assert_eq!(count_trigram(&corpus, ['e', ' ', 'l']), 210202);
-    assert_eq!(count_trigram(&corpus, ['†', ' ', 'l']), 227994);
-    assert_eq!(count_trigram(&corpus, ['e', 'a', 'h']), 6357);
-    assert_eq!(count_trigram(&corpus, ['†', 'a', 'h']), 24);
+    assert_eq!(corpus.count_trigram(['t', 'h', 'e']), 0);
+    assert_eq!(corpus.count_trigram(['t', 'h', '†']), 6802477);
+    assert_eq!(corpus.count_trigram(['h', 'e', ' ']), 0);
+    assert_eq!(corpus.count_trigram(['h', '†', ' ']), 5421447);
+    assert_eq!(corpus.count_trigram(['e', ' ', 'q']), 21049);
+    assert_eq!(corpus.count_trigram(['†', ' ', 'q']), 19454);
+    assert_eq!(corpus.count_trigram(['e', ' ', 'l']), 210202);
+    assert_eq!(corpus.count_trigram(['†', ' ', 'l']), 227994);
+    assert_eq!(corpus.count_trigram(['e', 'a', 'h']), 6357);
+    assert_eq!(corpus.count_trigram(['†', 'a', 'h']), 24);
 
     // Skipgrams
-    assert_eq!(count_skipgram(&corpus, ['t', 'e']), 1153321);
-    assert_eq!(count_skipgram(&corpus, ['t', '†']), 6802477);
-    assert_eq!(count_skipgram(&corpus, ['e', 'q']), 25582);
-    assert_eq!(count_skipgram(&corpus, ['†', 'q']), 19492);
-    assert_eq!(count_skipgram(&corpus, ['e', 'l']), 977172);
-    assert_eq!(count_skipgram(&corpus, ['†', 'l']), 323544);
+    assert_eq!(corpus.count_skipgram(['t', 'e']), 1153321);
+    assert_eq!(corpus.count_skipgram(['t', '†']), 6802477);
+    assert_eq!(corpus.count_skipgram(['e', 'q']), 25582);
+    assert_eq!(corpus.count_skipgram(['†', 'q']), 19492);
+    assert_eq!(corpus.count_skipgram(['e', 'l']), 977172);
+    assert_eq!(corpus.count_skipgram(['†', 'l']), 323544);
 
     // Everything else the same
-    // assert_eq!(count_bigram(&corpus, ['v', 'e']), 2978051);
-    // assert_eq!(count_bigram(&corpus, ['e', 'r']), 5219156);
-    // assert_eq!(count_bigram(&corpus, ['e', 'o']), 252440);
-    // assert_eq!(count_bigram(&corpus, ['e', 'i']), 291015);
-    // assert_eq!(count_bigram(&corpus, ['e', 'h']), 85972);
-    assert_eq!(count_trigram(&corpus, ['o', 'v', 'e']), 496824);
-    assert_eq!(count_trigram(&corpus, ['v', 'e', 'r']), 934355);
-    assert_eq!(count_trigram(&corpus, ['e', 'r', ' ']), 1580390);
-    assert_eq!(count_skipgram(&corpus, ['o', 'e']), 3660890);
+    // assert_eq!(corpus.count_bigram(['v', 'e']), 2978051);
+    // assert_eq!(corpus.count_bigram(['e', 'r']), 5219156);
+    // assert_eq!(corpus.count_bigram(['e', 'o']), 252440);
+    // assert_eq!(corpus.count_bigram(['e', 'i']), 291015);
+    // assert_eq!(corpus.count_bigram(['e', 'h']), 85972);
+    assert_eq!(corpus.count_trigram(['o', 'v', 'e']), 496824);
+    assert_eq!(corpus.count_trigram(['v', 'e', 'r']), 934355);
+    assert_eq!(corpus.count_trigram(['e', 'r', ' ']), 1580390);
+    assert_eq!(corpus.count_skipgram(['o', 'e']), 3660890);
 
     // Requires adjustments based on pentagrams to handle skips over invalid corpus chars
-    // assert_eq!(count_skipgram(&corpus, ['e', ' ']), 9021099);
+    // assert_eq!(corpus.count_skipgram(['e', ' ']), 9021099);
 }
 
 #[cfg(test)]
--- src/corpus_ext.rs
+++ src/corpus_ext.rs
@@ -8,6 +8,14 @@ pub(crate) trait CorpusExt {
     fn corpus_trigram(&mut self, trigram: &[char; 3]) -> usize;
     fn get_trigrams(&mut self) -> &mut Vec<u32>;
     fn get_skipgrams(&mut self) -> &mut Vec<u32>;
+    #[cfg(test)]
+    fn count_char(&self, c: char) -> u32;
+    #[cfg(test)]
+    fn count_bigram(&self, bg: [char; 2]) -> u32;
+    #[cfg(test)]
+    fn count_trigram(&self, tg: [char; 3]) -> u32;
+    #[cfg(test)]
+    fn count_skipgram(&self, sg: [char; 2]) -> u32;
 }
 
 impl CorpusExt for Corpus {
@@ -22,5 +30,25 @@ impl CorpusExt for Corpus {
     }
     fn get_skipgrams(&mut self) -> &mut Vec<u32> {
         &mut self.skipgrams
+    }
+
+    #[cfg(test)]
+    fn count_char(&self, c: char) -> u32 {
+        self.chars[self.corpus_char(c)]
+    }
+    
+    #[cfg(test)]
+    fn count_bigram(&self, bg: [char; 2]) -> u32 {
+        self.bigrams[self.corpus_bigram(&bg)]
+    }
+    
+    #[cfg(test)]
+    fn count_trigram(&self, tg: [char; 3]) -> u32 {
+        self.trigrams[self.corpus_trigram(&tg)]
+    }
+    
+    #[cfg(test)]
+    fn count_skipgram(&self, sg: [char; 2]) -> u32 {
+        self.skipgrams[self.corpus_bigram(&sg)]
     }
 }
